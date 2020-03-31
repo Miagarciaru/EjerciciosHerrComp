42946bb32bf1b89f9e83e186da4c7b7eb7e375ad
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2020-03-31 08:42:17 -0700

    plantilla del ejercicio

---
+++ Tareagrupal/sin.cpp
@@ -0,0 +1,25 @@
+//Implementar el método de Pade
+
+#include <iostream>
+#include <cmath>
+
+double sin_pade (double x);
+double sin_aux (double u);
+
+int main()
+{
+
+}
+
+double sin_pade (double x)
+{
+  if (std::abs(x)<=10e-8)
+    {
+      
+    }
+}
+
+double sin_aux (double u)
+{
+
+}
