384e288d9d76a9fb94ad3ef2c2205179c8e1ad7d
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2020-03-31 13:44:21 -0700

    Aunque no da igual que en el pdf, es bastante exacto

--- Tareagrupal/sin.cpp
+++ Tareagrupal/sin.cpp
@@ -12,12 +12,12 @@ int main()
   std::cout.precision(16);
   std::cout.setf(std::ios::scientific);
 
-  double x=0.0;
+  double x=0.01;
   double dx=0.01;
   
   for (int ii=1; x<=M_PI_2; x+=dx)
     {
-      std::cout<<x<<"\t"<<(std::abs(std::sin(x)-sin_pade(x))*100)/std::sin(x)<<"\n";
+      std::cout<<x<<"\t"<<std::abs(std::sin(x)-sin_pade(x))/std::sin(x)<<"\n";
     }
   
   return 0;
@@ -49,9 +49,9 @@ double sin_pade (double x)
 double sin_aux (double u)
 {
   double sinaux=0.0;
-  double num=(69212.0+(34911.0/110)*pow(u,4))-((29593.0/3)*pow(u,2)+(479249.0/166320)*pow(u,6));
+  double num=(5324.0+(34911.0/1430)*pow(u,4))-((29593.0/39)*pow(u,2)+(479249.0/2162169)*pow(u,6));
   double den=5324.0+(1671.0/13)*pow(u,2)+(97.0/66)*pow(u,4)+(2623.0/308880)*pow(u,6);
-  sinaux=(1.0/13)*u*(num/den);
+  sinaux=u*(num/den);
 
   return sinaux;
 }
